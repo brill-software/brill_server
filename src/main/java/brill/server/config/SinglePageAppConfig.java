5ca87272be31ea893024ae853b82531b47343d0f
Chris Bulcock <chris.bulcock@brill.software>
2023-12-05 15:25:14 +0000

    BRIL-119 License update and MIT change.

--- src/main/java/brill/server/config/SinglePageAppConfig.java
+++ src/main/java/brill/server/config/SinglePageAppConfig.java
@@ -1,4 +1,4 @@
-// © 2021 Brill Software Limited - Brill Framework, distributed under the Brill Software Proprietry License.
+// © 2021 Brill Software Limited - Brill Framework, distributed under the MIT License.
 package brill.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/brill/server/controller/AuthenticationController.java
+++ src/main/java/brill/server/controller/AuthenticationController.java
@@ -1,4 +1,4 @@
-// © 2021 Brill Software Limited - Brill Framework, distributed under the Brill Software Proprietry License.
+// © 2021 Brill Software Limited - Brill Framework, distributed under the MIT License.
 package brill.server.controller;
 
 import java.util.Map;
