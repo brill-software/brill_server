75f6ac97fc4fb70e7686c7af89c4f34865163f3c
Chris Bulcock <chris.bulcock@brill.software>
2024-03-12 12:06:53 +0000

    BRIL-163 Reverse commit dialog changes.

--- src/main/java/brill/server/controller/GitController.java
+++ src/main/java/brill/server/controller/GitController.java
@@ -523,6 +523,10 @@ public class GitController {
         try {
             topic = message.getString("topic");
             String commitBranch = gitService.getCurrentBranch(wsService.getWorkspace(session));
+            
+            JsonObject content = JsonUtils.getJsonObject(message, "content");
+            String commitMsg = JsonUtils.getString(content, "message");
+            boolean push = JsonUtils.getBoolean(content, "push");
 
             if (commitBranch.equals("master") || commitBranch.equals("develop")) {
                 wsService.sendErrorToClient(session, topic, "Protected Branch", 
@@ -530,11 +534,9 @@ public class GitController {
                 return;
             }
 
-            String commitMsg = JsonUtils.getString(message, "content");
+            
             wsService.sendErrorToClient(session, topic, "Updating", "Please wait while the respository is updated...", INFO_SEVERITY);
-            gitService.commitStagedFiles(wsService.getWorkspace(session), commitMsg, wsService.getName(session), wsService.getEmail(session));
-
-           
+            gitService.commitStagedFiles(wsService.getWorkspace(session), commitMsg, wsService.getName(session), wsService.getEmail(session), push);
 
             // Publish a list of commits to any sessions that has subscribed on the same branch as the commit.
             List<Subscriber> subscribers = wsService.getSubscribers("git:commits:/");
